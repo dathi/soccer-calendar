f60f6b9ae1bc176f39fc521edf6dcea5aac7f91f
dathi <dathi@tma.com.vn>
2015-09-24 19:39:55 +0700

    fix parsing match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -12,13 +12,13 @@ class Match < ActiveRecord::Base
     i = 1
 
     doc.css("div[data-type='evt']").each do |match|
-      puts "#{match.attr('data-esd')}|#{match.css('div.min').text.strip}|#{match.css('div.ply.name').text.strip}|#{match.css('div.sco > a.scorelink').text.strip}"
       home_guest = match.css('div.ply.name').text.strip.split('  ')
+      score = match.css('div.sco > a.scorelink').text.strip.presence || match.css('div.sco').text.strip
       matches << Match.new(:id => i,
                            :happen_time => match.attr('data-esd'),
                            :home => home_guest[0],
                            :guest => home_guest[1],
-                           :score => match.css('div.sco > a.scorelink').text.strip,
+                           :score => score,
                            :tournament => t)
       i = i.next
     end
